114c15112b74212764c43e65b138d8ce155edc55
Joanna Mei <jmei1@babson.edu>
2019-04-19 12:09:05 -0400

    added timidity/musescore tests, key class experimentation

--- testingmusic21_2.py
+++ testingmusic21_2.py
@@ -1,7 +1,16 @@
 from music21 import *
+#setting up the environment, connecting music21 to sheet music generator and MIDI player
 us = environment.UserSettings()
 us['musicxmlPath'] = '/home/joanna/.local/bin/MuseScore-3.0.5-x86_64.AppImage'
 us['midiPath'] = '/usr/bin/timidity'
 
+#testing built in classes and methods
 f = note.Note("F5")
-f.show('midi')
+print(f.name)
+print(f.octave)
+print(f.pitch)
+
+#testing sheet music generator
+#f.show()
+#testing MIDI player
+#f.show('midi')
